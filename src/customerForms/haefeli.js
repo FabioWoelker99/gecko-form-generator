8f21c5ef7107191ac22d0910790ab11a6638a90b
winno-GmbH <accounts@winno.ch>
2023-03-10 07:26:28 +0100

    initialized haefeli

---
+++ src/customerForms/haefeli.js
@@ -0,0 +1,89 @@
+const testForm = {
+    requestName: 'winno',
+    steps: [
+        {
+            label: 'Persönliche Daten',
+            stepId: 'personal',
+            rows: [
+                {
+                    elements: [
+                        {
+                            type: 'text',
+                            placeholder: 'Vorname',
+                            required: true,
+                            autocomplete: 'given-name',
+                            label: 'Name',
+                            name: 'surname',
+                        },
+                        {
+                            type: 'text',
+                            placeholder: 'Nachname',
+                            required: true,
+                            autocomplete: 'family-name',
+                            name: 'name',
+                        }
+                    ]
+                },
+                {
+                    elements: [
+                        {
+                            type: 'email',
+                            placeholder: 'max.muster@company.com',
+                            required: true,
+                            autocomplete: 'email',
+                            label: 'E-Mail',
+                            name: 'email',
+                        },
+                    ]
+                },
+                {
+                    elements: [
+                        {
+                            type: 'tel',
+                            placeholder: '+41 00 000 00 00',
+                            required: true,
+                            autocomplete: 'tel',
+                            label: 'Telefonnummer',
+                            name: 'telefon',
+                        },
+                    ]
+                },
+                {
+                    elements: [
+                        {
+                            type: 'textarea',
+                            placeholder: 'Ihre Nachricht oder Bemerkung',
+                            required: false,
+                            label: 'Nachricht',
+                            name: 'nachricht',
+                        },
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Privatumzug',
+            stepId: 'privatumzug',
+            rows: [
+                {
+                    elements: [
+                        {
+                            type: 'text',
+                            placeholder: 'Musterstrasse',
+                            required: true,
+                            label: 'Strasse',
+                            name: 'strasse',
+                        },
+                        {
+                            type: 'text',
+                            placeholder: 'Hausnummer',
+                            required: true,
+                            label: 'Hausnummer',
+                            name: 'hausnummer',
+                        }
+                    ]
+                },
+            ]
+        }
+    ]
+};
