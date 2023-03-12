ecf6b0cb3d9d1867aae8d74b0870819c3aae47c0
Fabio Woelker <fabio.woelker@coderevolution.de>
2023-03-12 17:37:52 +0100

    finished bundling

---
+++ dist/GeckoFormListener.js
@@ -0,0 +1,8 @@
+class GeckoFormListener {
+  constructor(geckoForm) {
+    this.geckoForm = geckoForm;
+  }
+  addSubmitListener() {
+    $(`${this.geckoForm.submitButtonSelector}`).on('click', this.geckoForm.moveToNextStep.bind(this.geckoForm));
+  }
+}
