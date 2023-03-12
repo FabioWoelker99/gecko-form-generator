a5af532dcdea6fa154fc0ca34f229daedc81645a
Fabio Woelker <fabio.woelker@coderevolution.de>
2023-03-12 18:02:27 +0100

    delete dist

--- dist/GeckoFormListener.js
+++
@@ -1,8 +0,0 @@
-class GeckoFormListener {
-  constructor(geckoForm) {
-    this.geckoForm = geckoForm;
-  }
-  addSubmitListener() {
-    $(`${this.geckoForm.submitButtonSelector}`).on('click', this.geckoForm.moveToNextStep.bind(this.geckoForm));
-  }
-}
