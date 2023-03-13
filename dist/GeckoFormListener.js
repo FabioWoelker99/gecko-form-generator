e02e4745f1740260b3f9d205b724ad813896f687
Fabio Woelker <fabio.woelker@coderevolution.de>
2023-03-13 10:18:40 +0100

    test

--- dist/GeckoFormListener.js
+++ dist/GeckoFormListener.js
@@ -6,15 +6,17 @@ class GeckoFormListener {
     $(`${this.geckoForm.submitButtonSelector}`).on('click', this.geckoForm.geckoFormManipulator.moveToNextStep.bind(this.geckoForm.geckoFormManipulator));
   }
   addRadioTriggerListener(json) {
-    $(document).on('change', `${this.geckoForm.formSelector} input[type="radio"][name="${json.name}"]`, function () {
-      const value = this.value;
-      console.log(value);
+    $(document).on('change', `${this.geckoForm.formSelector} input[type="radio"][name="${json.name}"]`, ev => {
+      const value = ev.currentTarget.value;
+      console.log(ev.value);
+      console.log(ev.currentTarget.value);
+      console.log($(ev.currentTarget.value));
+
+      // delete step groups
 
       // add step groups
       const steps = this.geckoForm.formJson.steps.filter(step => step.stepGroup == value);
       this.geckoForm.geckoFormManipulator.buildGeckoSteps(steps);
-
-      // delete step groups
     });
   }
 }
