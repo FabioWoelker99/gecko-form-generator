45fe6046f471e426ad31aab645e7bea5c9ffab03
Fabio Woelker <fabio.woelker@coderevolution.de>
2023-03-17 03:50:04 +0100

    add new label

--- src/customerForms/haefeli/executioner.js
+++ src/customerForms/haefeli/executioner.js
@@ -1,3 +1,3 @@
 
-const geckoForm = new GeckoForm(geckoHaefeliForm, '#test-gecko-form', '.wr_btn--submit', '.wr_btn--form-back', '.lyt--form-steps', '.main', 'Weiter', 'Absenden');
+const geckoForm = new GeckoForm(geckoHaefeliForm, '#test-gecko-form', '.wr_btn--submit', '.wr_btn--form-back', '.lyt--form-steps', '.main', 'Weiter', 'Absenden', 'Speichern & Weiter');
 geckoForm.buildGeckoForm();
