2c6697c9318995108a528c45504b67e1d232c791
Fabio Woelker <fabio.woelker@coderevolution.de>
2023-03-19 13:30:23 +0100

    deploy

--- dist/GeckoFormManipulator.js
+++ dist/GeckoFormManipulator.js
@@ -215,7 +215,7 @@ class GeckoFormManipulator {
   resetForm(manipulator) {
     $(`${manipulator.geckoForm.formSelector}`).html('');
     $(`${manipulator.geckoForm.formStepsSelector}`).html('');
-    const newForm = new GeckoForm(manipulator.geckoForm.formJson, manipulator.geckoForm.formSelector, manipulator.geckoForm.submitButtonSelector, manipulator.geckoForm.backButtonSelector, manipulator.geckoForm.formStepsSelector, manipulator.geckoForm.messageContainerSelector);
+    const newForm = new GeckoForm(manipulator.geckoForm.formJson, manipulator.geckoForm.formSelector, manipulator.geckoForm.submitButtonSelector, manipulator.geckoForm.backButtonSelector, manipulator.geckoForm.formStepsSelector, manipulator.geckoForm.messageContainerSelector, manipulator.geckoForm.fowardButtonLabel, manipulator.geckoForm.sendButtonLabel, manipulator.geckoForm.fowardSafeButtonLabel);
     newForm.buildGeckoForm();
   }
 }
