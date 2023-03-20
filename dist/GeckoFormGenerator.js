6a98896cb275ae740304d3cc3451b645c9510a0c
Besson <yanic.besson@gmail.com>
2023-03-20 10:39:52 +0100

    runned npm run build

--- dist/GeckoFormGenerator.js
+++ dist/GeckoFormGenerator.js
@@ -1,6 +1,7 @@
 class GeckoFormGenerator {
   constructor(geckoForm) {
     this.geckoForm = geckoForm;
+    this.counter = 0;
   }
   buildSingleGeckoStepView(json, position) {
     let content = '';
@@ -50,9 +51,11 @@ class GeckoFormGenerator {
     content += `<div class="${gecko_class_formRowLayout} lyt">`;
     json.elements.forEach(element => {
       content += this.generateSingleFormItem(element);
+      this.counter += 1;
     });
     content += '</div>';
     content += '</div>';
+    this.counter = 0;
     return content;
   }
   generateSingleFormItem(json) {
@@ -139,10 +142,11 @@ class GeckoFormGenerator {
     const placeholder = json.placeholder ? `placeholder="${json.placeholder}"` : '';
     const required = json.required ? `required="${json.required}"` : '';
     const name = json.name ? `name="${json.name}"` : '';
+    const autofocus = this.counter === 0 ? "autofocus" : "";
     const label = json.label ?? '';
     let content = '';
     content += `<p class="${gecko_class_label}">${label}</p>`;
-    content += `<input class="${gecko_class_inputElement} ${gecko_class_inputGeneralElement}" type="${json.type}" ${placeholder} ${required} ${autocomplete} ${name}>`;
+    content += `<input class="${gecko_class_inputElement} ${gecko_class_inputGeneralElement}" type="${json.type}" ${placeholder} ${required} ${autocomplete} ${name} ${autofocus}>`;
     this.geckoForm.geckoFormListener.addRealtimeValidationListener(json);
     return content;
   }
