cee5a3efd677f83aec718e8de1240478093a113b
Fabio Woelker <fabio.woelker@coderevolution.de>
2023-07-04 00:57:23 +0200

    bugfix

--- dist/customerForms/multitransport-quick-1/executioner.js
+++ dist/customerForms/multitransport-quick-1/executioner.js
@@ -1,2 +1,2 @@
-const mtq1geckoForm = new GeckoForm(geckoMultitransportFormq1, '#mt-quick-form-1', '.wr_btn--submit.quick-1', '.wr_btn--form-back.quick-1', '.lyt--form-steps.quick-1', '.main', 'Weiter', 'Absenden', 'Speichern & Weiter');
+const mtq1geckoForm = new GeckoForm(geckoMultitransportFormq1, '#mt-quick-form-1', '.wr_btn--submit.quick-1', '.wr_btn--form-back.quick', '.lyt--form-steps.quick', '.main', 'Weiter', 'Absenden', 'Speichern & Weiter');
 mtq1geckoForm.buildGeckoForm();
