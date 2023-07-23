98e717ed4da65206fbfed4dd1b23875d3ce91f8b
winno-GmbH <accounts@winno.ch>
2023-07-23 04:17:11 +0200

    Finasa Model Form added

---
+++ src/customerForms/finasa/executioner.js
@@ -0,0 +1,3 @@
+
+const geckoForm = new GeckoForm(geckoFinasaForm, '#test-gecko-form', '.wr_btn--submit', '.wr_btn--form-back', '.lyt--form-steps', '.main', 'Next', 'Submit', 'Save & Next');
+geckoForm.buildGeckoForm();
